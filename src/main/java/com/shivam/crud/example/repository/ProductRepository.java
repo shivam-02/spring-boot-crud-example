85b6abd0c621223fe44dc2086513ca5fcf25a311
Shivam Sugandhi <shivam.sugandhi13@gmail.com>
2023-05-10 11:44:36 -0700

    First commit

---
+++ src/main/java/com/shivam/crud/example/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.shivam.crud.example.repository;
+
+import com.shivam.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
---
+++ src/test/java/com/shivam/crud/example/SpringBootCrudExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.crud.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
